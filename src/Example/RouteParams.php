ad13560b0cefe9919c448c1c3df17c55e8b85e9f
Cameron <cameronjohdnavis@gmail.com>
2015-05-31 21:29:07 +0100

    Example route generation

--- src/Example/RouteParams.php
+++ src/Example/RouteParams.php
@@ -31,12 +31,15 @@ class RouteParams
     {
         $intParam = $this->router->getParams()['intParam'];
         $param2 = $this->router->getParams()['param2'];
+        
+        $generated = $this->router->generate(\ExampleRoutes::ROUTE_PARAMS, ['intParam' => 666, 'param2' => 'p2']);
 
         $page = $this->pageBuilder->create()
                 ->setBody("<h1>Example route parameters</h1>
                            <ul>
                             <li>intParam: {$intParam}</li>
                             <li>param2: {$param2}</li>
+                            <li>Generated route <a href=\"?r={$generated}\">$generated</a></li>
                            </ul>
                            <p>Controller action was <em>" . __METHOD__ . "();</em></p>");
 
