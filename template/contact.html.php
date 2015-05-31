a0662b48ae29c821815d3d7c7c80657e381ddc74
Cameron <cameronjohdnavis@gmail.com>
2015-05-31 12:15:43 +0100

    Deleted unused files

--- template/contact.html.php
+++
@@ -1,4 +0,0 @@
-<ul>
-    <li><a href="?r=<?= $contact ?>&intVal=123&boolVal=0">Contact again</a></li>
-    <li>$exampleClass: <?= var_dump($exampleClass) ?></li>
-</ul>
--- template/debug.html.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-        <title>Debug page</title>
-    </head>
-    <body>
-        <p>This template was set in config.dev.php to override the config.php setting.</p>
-    </body>
-</html>
